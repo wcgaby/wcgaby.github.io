aa71f9f814d670afad72615f82a79789b76ac57d
wcgaby <69859911+wcgaby@users.noreply.github.com>
2020-11-05 10:20:19 -0500

    project 2 for class push

--- project-2/styles/multiple-buttons/assets/multi-button.js
+++ project-2/styles/multiple-buttons/assets/multi-button.js
@@ -1,33 +1,33 @@
 // Mutli-button.js that allows a user to click a button to show one item while hiding other items.
 $(document).ready(function() {
   //ID or Class name of the button goes into the first section
-  $('#bow-button').click(function() {
+  $('#archery-button').click(function() {
     //ID or Class name of the modal you want to appear on click goes into this second area.
-    $("#bow-calendar").removeClass("invisible");
-    $("#shotgun-calendar").addClass("invisible");
+    $("#archery-calendar").removeClass("invisible");
+    $("#muzzleloader-calendar").addClass("invisible");
     $("#rifle-calendar").addClass("invisible");
-    $("#bow-button").addClass("active");
-    $("#shotgun-button").removeClass("active");
+    $("#archery-button").addClass("active");
+    $("#muzzleloader-button").removeClass("active");
     $("#rifle-button").removeClass("active");
   });
 
-  $('#shotgun-button').click(function() {
+  $('#muzzleloader-button').click(function() {
     //ID or Class name of the modal you want to appear on click goes into this second area.
-    $("#bow-calendar").addClass("invisible");
-    $("#shotgun-calendar").removeClass("invisible");
+    $("#archery-calendar").addClass("invisible");
+    $("#muzzleloader-calendar").removeClass("invisible");
     $("#rifle-calendar").addClass("invisible");
-    $("#bow-button").removeClass("active");
-    $("#shotgun-button").addClass("active");
+    $("#archery-button").removeClass("active");
+    $("#muzzleloader-button").addClass("active");
     $("#rifle-button").removeClass("active");
   });
 
   $('#rifle-button').click(function() {
     //ID or Class name of the modal you want to appear on click goes into this second area.
-    $("#bow-calendar").addClass("invisible");
-    $("#shotgun-calendar").addClass("invisible");
+    $("#archery-calendar").addClass("invisible");
+    $("#muzzleloader-calendar").addClass("invisible");
     $("#rifle-calendar").removeClass("invisible");
-    $("#bow-button").removeClass("active");
-    $("#shotgun-button").removeClass("active");
+    $("#archery-button").removeClass("active");
+    $("#muzzleloader-button").removeClass("active");
     $("#rifle-button").addClass("active");
   });
 });
